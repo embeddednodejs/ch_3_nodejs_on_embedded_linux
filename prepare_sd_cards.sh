6a4b666ca3dd99429a16e57d6959b31756daea11
Patrick Mulder <mulder.patrick@gmail.com>
2016-10-15 18:29:45 +0200

    notes on preparing sd card

---
+++ prepare_sd_cards.sh
@@ -0,0 +1,13 @@
+
+# list disks mac os
+
+diskutil list 
+
+# diskutil unmountDisk /dev/diskN
+
+# copy image to disk
+#  dd if=iot-devkit-YYYYMMDDHHMM-mmcblkp0.direct of=/dev/disk1 bs=8m
+
+
+# windows users can use tools like Rufus for example
+
